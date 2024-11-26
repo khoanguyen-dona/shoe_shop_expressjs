e45df97061fb887acdcce7e2eec316b65460898a
khoanguyen-dona <khoa.nguyen.anh8796@gmail.com>
2024-11-26 21:15:38 +0700

    api feature:login,register

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+dotenv.config()
 // const userRoute = require('./routes/user')
 // const cartRoute = require('./routes/cart')
 // const orderRoute = require('./routes/order')
 // const productRoute = require('./routes/product')
 const authRoute = require('./routes/auth')
+const cors = require('cors')
 
 
-dotenv.config()
 
 mongoose.connect(process.env.MONGO_DB)
         .then(() => console.log("DB connect successfully"))
@@ -19,9 +20,10 @@ app.listen(process.env.PORT ,() => {
     console.log('backend is running');
 } )
 
+app.use(cors());
 app.use(express.json());
-// app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
+// app.use('/api/user', userRoute);
 // app.use('/api/cart', cartRoute);
 // app.use('/api/order', orderRoute);
 // app.use('/api/product', productRoute);
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 
 const User = require('../models/User')
-
 const router = require('express').Router()
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
 
 router.post('/register', async (req, res) => {
     const newUser = new User({
@@ -18,5 +20,32 @@ router.post('/register', async (req, res) => {
         res.status(500).json(err);
     }
 })
+
+router.post('/login', async(req, res) => {
+    try{
+
+        const user = await User.findOne({email: req.body.email})
+        if(!user){
+            return res.status(400).json('Sai email hoặc mật khẩu')
+        }
+        
+        if( user.password!== req.body.password ){
+            return res.status(400).json('Sai email hoặc mật khẩu')
+        }
+        const accessToken = jwt.sign(
+            {
+              id: user._id,
+              isAdmin: user.isAdmin,
+            },
+            process.env.JWT_SECRET_KEY,
+            {expiresIn:"1d"}
+          );
+        const { password, ...others} = user._doc
+        res.status(200).json({...others, accessToken})
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
 
 module.exports = router
