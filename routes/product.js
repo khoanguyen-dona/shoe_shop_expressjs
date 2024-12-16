4bbbb26214327ce8df313f901d57fb830ea84cab
khoanguyen-dona <khoa.nguyen.anh8796@gmail.com>
2024-12-16 15:14:57 +0700

    api feature: get product with pagination

--- routes/product.js
+++ routes/product.js
@@ -4,29 +4,64 @@ const router = require('express').Router()
 
 // get product with id
 router.get('/:id', async (req, res) => {
+
     try{
-        const productId = req.params.id
+        const productId = req.params.id    
         const product = await Product.findById(productId)
         res.status(200).json(product) 
-
+    
     }catch(err){
         res.status(500).json(err)
     }
 })
 
 // get products base on category
-router.get('', async (req, res) => {
+// router.get('', async (req, res) => {
     
+//     try {
+//         qCategory = req.query.category
+//         if(qCategory){
+//             const products = await Product.find({ categories: {
+//                 $in: [qCategory]
+//             } })          
+//             res.status(201).json(products);
+//         } else {
+//             const products = await Product.find()           
+//             res.status(201).json(products);
+//         }      
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// })
+
+// get products base on category with pagination
+router.get('', async (req, res) => {
+    const page = parseInt(req.query.page) 
+    const limit = parseInt(req.query.limit) 
+
+    const startIndex = (page - 1) * limit 
+    const endIndex = (startIndex + limit) 
     try {
         qCategory = req.query.category
-        if(qCategory){
-            const products = await Product.find({ categories: {
-                $in: [qCategory]
-            } })          
-            res.status(201).json(products);
-        } else {
-            const products = await Product.find()           
-            res.status(201).json(products);
+        if (qCategory) {
+            console.log(qCategory)
+            qCategory=qCategory.split(',')
+            const products = await Product.find({
+                categories: { $in: qCategory}
+            })   
+            const paginated_products = products.slice(startIndex,endIndex) 
+            const totalPage = Math.ceil(products.length / limit)
+            const totalProducts = products.length
+            res.status(200).json({message:'get products successfully', totalProducts: totalProducts, page: page,
+                totalPage: totalPage, limit: limit, startIndex: startIndex, endIndex: endIndex, products: paginated_products});
+        } 
+        else {
+            const products = await Product.find()     
+            const paginated_products = products.slice(startIndex,endIndex) 
+            const totalPage = Math.ceil(products.length / limit)
+            const totalProducts = products.length
+            res.status(200).json({message:'get products successfully', totalProducts: totalProducts, page: page,
+                totalPage: totalPage, limit: limit, startIndex: startIndex, endIndex: endIndex, products: paginated_products});
         }      
     } catch (err) {
         res.status(500).json(err);
